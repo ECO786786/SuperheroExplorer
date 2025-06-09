4d018b984cc1793498ed17f15a563c109655bfb6
Emma Collins <lowpee56@hotmail.com>
2025-06-09 12:45:52 +0100

    update selection options

--- src/SelectOptions.jsx
+++ src/SelectOptions.jsx
@@ -1,13 +1,23 @@
+import React, { memo } from "react";
+
 const SelectOptions = ({
   handlePublisherChange,
   selectedPublisher,
   publishers,
 }) => {
+  const sortedPublishers = [...publishers].sort();
+
   return (
-    <div>
-      <select onChange={handlePublisherChange} value={selectedPublisher}>
+    <div className="select-options-container">
+      <label htmlFor="publisher-select">Filter by Publisher:</label>
+      <select
+        id="publisher-select"
+        onChange={handlePublisherChange}
+        value={selectedPublisher}
+        className="publisher-select"
+      >
         <option value="">All Publishers</option>
-        {publishers.map((pub) => (
+        {sortedPublishers.map((pub) => (
           <option key={pub} value={pub}>
             {pub}
           </option>
@@ -17,4 +27,4 @@ const SelectOptions = ({
   );
 };
 
-export default SelectOptions;
+export default memo(SelectOptions);
